a8da0c6e235a2b0a26e2f4c7a7a551b113f33efd
Jordan-Castillo <jtcastil@calpoly.edu>
2017-11-23 21:59:48 -0800

    additional updates

--- BAKERY-queries.sql
+++ BAKERY-queries.sql
@@ -6,7 +6,7 @@
 
 * Creation Date : 20-11-2017
 
-* Last Modified : Mon 20 Nov 2017 09:27:00 PM STD
+* Last Modified : Thu 23 Nov 2017 09:59:37 PM STD
 
 * Created By :  Jordan Castillo
 
@@ -45,7 +45,7 @@ WHERE CId NOT IN
          ON it.Item = gd.GId
 		WHERE MONTHNAME(rc.SaleDate) = "October"
       AND YEAR(rc.SaleDate) = 2007
-		AND gd.Food = "Twist")
+		AND gd.Food = "Twist");
 
 -- Q3 IN SUBQUERY, GROUPBY FOODTYPE,FLAVOR, FIND TOTAL REVENUE, FIND MAX IN OUTER QUERY
 SELECT Flavor, Food, revenueContribution
@@ -60,10 +60,79 @@ FROM (SELECT gd.Flavor, gd.Food,
          ON it.Item = gd.GId
 		GROUP BY gd.Flavor, gd.Food
 		ORDER BY revenueContribution DESC) AS myTbl
-HAVING MAX(revenueContribution)
+HAVING MAX(revenueContribution);
 
--- Q4
+-- Q4 IN SUBQUERY, GROUP BY FOODTYPE, FLAVOR, FIND TOTAL PURCHASES, FIND MAX IN OUTER QUERY
+SELECT Flavor, Food, totalPurchases
+FROM (SELECT gd.Flavor, gd.Food,
+             SUM(it.Ordinal) AS totalPurchases
+      FROM customers cm
+      JOIN receipts rc
+         ON cm.CId = rc.Customer
+      JOIN items it
+         ON rc.RNumber = it.Receipt
+      JOIN goods gd
+         ON it.Item = gd.GId
+      GROUP BY gd.Flavor, gd.Food
+      ORDER BY totalPurchases DESC) AS myTbl
+HAVING MAX(totalPurchases);
 
+-- Q5 IN SUBQUERY, GROUP BY DAY(), LIMIT MONTH&YEAR,
+SELECT SaleDate, revenueContribution
+FROM (SELECT rc.SaleDate, 
+		ROUND(SUM(it.Ordinal * gd.PRICE), 2) AS revenueContribution
+      FROM customers cm
+      JOIN receipts rc
+         ON cm.CId = rc.Customer
+      JOIN items it
+         ON rc.RNumber = it.Receipt
+      JOIN goods gd
+         ON it.Item = gd.GId
+      WHERE MONTHNAME(rc.SaleDate) = "October"
+      AND YEAR(rc.SaleDate) = 2007
+		GROUP BY DAY(rc.SaleDate)
+		ORDER BY revenueContribution DESC) AS myTbl
+HAVING MAX(revenueContribution);
+
+-- Q6 
+SELECT 
+FROM customers cm
+	JOIN receipts rc
+		ON cm.CId = rc.Customer
+	JOIN items it
+		ON rc.RNumber = it.Receipt
+	JOIN goods gd
+		ON it.Item = gd.GId
+WHERE rc.SaleDate IN (
+	SELECT SaleDate
+	FROM (SELECT rc.SaleDate,
+	      ROUND(SUM(it.Ordinal * gd.PRICE), 2) AS revenueContribution
+	      FROM customers cm
+	      JOIN receipts rc
+	         ON cm.CId = rc.Customer
+	      JOIN items it
+	         ON rc.RNumber = it.Receipt
+	      JOIN goods gd
+	         ON it.Item = gd.GId
+	      WHERE MONTHNAME(rc.SaleDate) = "October"
+	      AND YEAR(rc.SaleDate) = 2007
+	      GROUP BY DAY(rc.SaleDate)
+	      ORDER BY revenueContribution DESC) AS myTbl
+	HAVING MAX(revenueContribution))
+GROUP BY gd.Flavor, gd.Food
+-- Q7
+
+
+
+-- Q8
+
+
+
+-- Q9
+
+
+
+-- Q10
 
 
 
